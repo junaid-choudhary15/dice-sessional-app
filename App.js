bc2b52f45439e5c00a72055ac00a07b5ed451e42
junaidhd <muhammadjunaidzahidhsp@gmail.com>
2020-12-05 19:34:01 +0500

    Commiting app

--- App.js
+++ App.js
@@ -1,21 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  Text,
+  StyleSheet,
+  View,
+  InputText,
+  TouchableOpacity,
+} from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import SplashScreen from "react-native-splash-screen";
+
+import profile from "/screens/profile";
+import dice from "./screen/dice";
+import Imagescreen from "./screens/drawer";
+import listscreen from "./screens/splashscreen";
+
+import DrawerNavigation from "./src/pages/drawer";
+
+const Stack = createStackNavigator();
+
+export default class App extends React.Component {
+  state = {
+    authenticated: false,
+  };
+
+  setAuth = (action) => {
+    this.setState({ authenticated: action });
+  };
+
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        {/* <StatusBar backgroundColor="#0D2C54" /> */}
+        <NavigationContainer>
+          {this.state.authenticated === true ? (
+            <DrawerNavigation logout={this.setAuth} />
+          ) : (
+            <Stack.Navigator initialRouteName="splashscreen">
+              <Stack.Screen
+                name="splashscreen"
+                component={Welcome}
+                Image={Imagescreen}
+                profile={ProfileScreen}
+                initialParams={{ setAuth: this.setAuth }}
+              />
+              <Stack.Screen name="dice" component={dice} />
+            </Stack.Navigator>
+          )}
+        </NavigationContainer>
+      </View>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const mystyles = StyleSheet.create({});
